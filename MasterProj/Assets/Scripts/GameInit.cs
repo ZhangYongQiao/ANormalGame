7fbbf32655fc035de7ea61a1821d14a423194fb3
zyq <1348808364@qq.com>
2022-04-26 22:59:00 +0800

    fix issue

--- MasterProj/Assets/Scripts/GameInit.cs
+++ MasterProj/Assets/Scripts/GameInit.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class GameInit : MonoBehaviour
@@ -32,7 +33,25 @@ public class GameInit : MonoBehaviour
     IEnumerator LoadAtlas()
     {
         string path = PathMgr.AtlasAbPath + "/atlasab";
+        AssetBundleCreateRequest abcr = AssetBundle.LoadFromFileAsync(path);
+        yield return abcr;
+        if (abcr.isDone)
+        {
+            var ab = abcr.assetBundle;
+            Texture go = ab.LoadAsset<Texture>("mingren");
+            if (!go)
+            {
+                Debug.LogError($"º”‘ÿUserGo ß∞‹");
+                yield return null;
+            }
 
+            //Sprite[] sprs = ab.LoadAssetWithSubAssets<Sprite>("mingren");
+            //foreach (var item in sprs)
+            //{
+            //    Debug.Log(item.name);
+            //}
+        }
+        StopCoroutine(LoadAtlas());
     }
 
 }
